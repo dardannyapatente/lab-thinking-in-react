5a4015cb6dfa05cc1b01e818bd11369f3071e7f7
Dardannya Patente <dardannyapatente@hotmail.com>
2021-03-17 10:40:34 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import data from './data';
+
+import FilterableProductTable from './components/FilterableProductTable';
+
+const products = data.data;
 
 function App() {
   return (
     <div className="App">
-
+      <FilterableProductTable products={products} />
     </div>
   );
 }
